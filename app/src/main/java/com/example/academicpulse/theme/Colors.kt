e0d5b81549c3db46f3a296a490860c38d01ffc74
dimahmz <essaadihamza4@outlook.com>
2024-04-20 19:52:27 +0100

    colors schema in light mode (and dark mode)

--- app/src/main/java/com/example/academicpulse/theme/Colors.kt
+++ app/src/main/java/com/example/academicpulse/theme/Colors.kt
@@ -4,17 +4,14 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val green = Color(0xFF0B8AA0)
+val blue = Color(0xFFEAF2FF)
+val white = Color(0xFFFFFFFF)
 
 val danger = Color(0xFFD70000)
-val surface_primary = Color(0xFFFFFFFF)
-val surface_secondary = Color(0xFFEAF2FF)
+val brown = Color(0xFF655D53)
+val primary_background = Color(0xFFFFFFFF)
+val secondary_background = Color(0xFFEAF2FF)
 val primary = Color(0xFF0B8AA0)
 val border_color = Color(0xFFC5C6CC)
 val fonts_color_header = Color(0xFF131314)
@@ -22,13 +19,13 @@ val fonts_color_paragraph = Color(0xFF2F3036)
 val fonts_color_text = Color(0xFF71727A)
 
 val DarkColorScheme = darkColorScheme(
-	primary = Purple80,
-	secondary = PurpleGrey80,
-	tertiary = Pink80
+	primary = green,
+	secondary = blue,
+	tertiary = white,
 )
 
 val LightColorScheme = lightColorScheme(
-	primary = Purple40,
-	secondary = PurpleGrey40,
-	tertiary = Pink40
+	primary = green,
+	secondary = blue,
+	tertiary = white,
 )
