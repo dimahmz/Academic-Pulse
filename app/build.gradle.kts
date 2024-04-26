718e0288fc405f64e16434081bfd15fbad8f9958
Saadaoui Oussama <saadaoui.oussama.003@gmail.com>
2024-04-26 15:45:28 +0100

    Configure the app loading page (Splash Screen)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,6 @@ android {
 }
 
 dependencies {
-
 	implementation("androidx.core:core-ktx:1.12.0")
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 	implementation("androidx.activity:activity-compose:1.8.2")
@@ -57,7 +56,10 @@ dependencies {
 	implementation("androidx.compose.ui:ui-graphics")
 	implementation("androidx.compose.ui:ui-tooling-preview")
 	implementation("androidx.compose.material3:material3")
+
+	// Navigation
 	implementation("androidx.navigation:navigation-compose:2.7.7")
+	implementation("androidx.core:core-splashscreen:1.0.1")
 
 	// Testing & Debugging
 	testImplementation("junit:junit:4.13.2")
