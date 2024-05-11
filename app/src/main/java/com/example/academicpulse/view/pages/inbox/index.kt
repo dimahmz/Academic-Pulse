543f4cf37c1f790a7ff6e1c973218758e7631173
Saadaoui Oussama <saadaoui.oussama.003@gmail.com>
2024-05-12 00:11:59 +0100

    Remove anything related to inbox

--- app/src/main/java/com/example/academicpulse/view/pages/inbox/index.kt
+++
@@ -1,9 +0,0 @@
-package com.example.academicpulse.view.pages.inbox
-
-import androidx.compose.runtime.Composable
-import com.example.academicpulse.view.components.basic.Text
-
-@Composable
-fun InboxPage() {
-	Text(text = "Inbox Page")
-}
