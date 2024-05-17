71d5c85e948562b6b3a71e20336e80c8eee7b899
Saadaoui Oussama <saadaoui.oussama.003@gmail.com>
2024-05-17 05:36:40 +0100

    TODO

--- app/src/main/java/com/example/academicpulse/model/PublicationType.kt
+++ app/src/main/java/com/example/academicpulse/model/PublicationType.kt
@@ -2,6 +2,7 @@ package com.example.academicpulse.model
 
 data class PublicationType(val id: String, val name: String) {
 	companion object {
+		// TODO: fill the list with real data!
 		val list = arrayListOf(
 			PublicationType("ar", "Arabic"),
 			PublicationType("en", "English"),
