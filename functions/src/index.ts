f2ae377689d76ebbfb2810f9067b7d7d4eebc33c
dimahmz <essaadihamza4@outlook.com>
2024-06-25 21:18:35 +0100

    verify every new publication when it's created automatically

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,15 +2,23 @@
 /* eslint-disable max-len */
 // The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
 import * as v2 from "firebase-functions/v2";
-import {getFirestore} from "firebase-admin/firestore";
+import { getFirestore } from "firebase-admin/firestore";
 // The Firebase Admin SDK to access Firestore.
-import {initializeApp} from "firebase-admin/app";
+import { initializeApp } from "firebase-admin/app";
 import {
   onDocumentCreated,
   onDocumentDeleted,
 } from "firebase-functions/v2/firestore";
+import * as dotenv from "dotenv";
+import axios from "axios";
+
+// Load environment variables from .env file
+dotenv.config();
 
 initializeApp();
+
+const VERIFY_PUBLICATION_API =
+  "http://192.168.8.103:3000/publications/automate/status";
 
 // Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
@@ -20,10 +28,10 @@ export const addmessage = v2.https.onRequest(async (req, res) => {
   // Push the new message into Firestore using the Firebase Admin SDK.
   const writeResult = await getFirestore()
     .collection("messages")
-    .add({original: original});
+    .add({ original: original });
 
   // Send back a message that we've successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} is added.`});
+  res.json({ result: `Message with ID: ${writeResult.id} is added.` });
 });
 
 // delete the publication's ID from the user document when its deleted
@@ -52,7 +60,27 @@ export const onDeletePublication = onDocumentDeleted(
         (pubId: string) => pubId !== event.params.docId
       );
 
-      await userRef.update({publications: filteredPublications});
+      await userRef.update({ publications: filteredPublications });
     }
+  }
+);
+
+// Automatically verify new publication
+export const onCreatePublication = onDocumentCreated(
+  "publication/{docId}",
+  async (event) => {
+    const snapshot: any = event.data;
+    if (!snapshot) {
+      return;
+    }
+
+    const pubId: string = event.params.docId;
+
+    if (process.env.VERIFY_PUBLICATION_API) return;
+
+    axios
+      .get(`${process.env.VERIFY_PUBLICATION_API}/${pubId}`)
+      .then()
+      .catch((e) => console.error(e.message));
   }
 );
