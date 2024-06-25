cb3ac3ef7586e215f2913e78eafb4184a3d93f4b
dimahmz <essaadihamza4@outlook.com>
2024-06-25 18:46:28 +0100

    start using firebase cloud functions

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,27 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+import * as v2 from "firebase-functions/v2";
+import {getFirestore} from "firebase-admin/firestore";
+// The Firebase Admin SDK to access Firestore.
+import {initializeApp} from "firebase-admin/app";
+import {
+  onDocumentCreated,
+  onDocumentDeleted,
+} from "firebase-functions/v2/firestore";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// Take the text parameter passed to this HTTP endpoint and insert it into
+// Firestore under the path /messages/:documentId/original
+export const addmessage = v2.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original: any = req.query.text;
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await getFirestore()
+    .collection("messages")
+    .add({original: original});
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  // Send back a message that we've successfully written the message
+  res.json({result: `Message with ID: ${writeResult.id} is added.`});
+});
