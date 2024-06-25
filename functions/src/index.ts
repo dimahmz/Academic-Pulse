ea5619b18a880f8c5f3ef9c5326c91b3eab9bfa4
dimahmz <essaadihamza4@outlook.com>
2024-06-25 21:24:56 +0100

    clean the index file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,8 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable max-len */
-// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+
 import * as v2 from "firebase-functions/v2";
 import { getFirestore } from "firebase-admin/firestore";
-// The Firebase Admin SDK to access Firestore.
 import { initializeApp } from "firebase-admin/app";
 import {
   onDocumentCreated,
@@ -17,21 +16,10 @@ dotenv.config();
 
 initializeApp();
 
-const VERIFY_PUBLICATION_API =
-  "http://192.168.8.103:3000/publications/automate/status";
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-export const addmessage = v2.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original: any = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await getFirestore()
-    .collection("messages")
-    .add({ original: original });
-
-  // Send back a message that we've successfully written the message
-  res.json({ result: `Message with ID: ${writeResult.id} is added.` });
+// text function
+export const textapi = v2.https.onRequest(async (req, res) => {
+  res.json({ message: `hello world` });
 });
 
 // delete the publication's ID from the user document when its deleted
@@ -65,6 +53,7 @@ export const onDeletePublication = onDocumentDeleted(
   }
 );
 
+
 // Automatically verify new publication
 export const onCreatePublication = onDocumentCreated(
   "publication/{docId}",
@@ -76,7 +65,7 @@ export const onCreatePublication = onDocumentCreated(
 
     const pubId: string = event.params.docId;
 
-    if (process.env.VERIFY_PUBLICATION_API) return;
+    if (!process.env.VERIFY_PUBLICATION_API) return;
 
     axios
       .get(`${process.env.VERIFY_PUBLICATION_API}/${pubId}`)
