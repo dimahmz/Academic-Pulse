e74c8e35afc58e316fe58452ff0a67b1cd5d65e6
dimahmz <essaadihamza4@outlook.com>
2024-10-13 04:28:21 +0100

    add item not found

--- app/src/main/java/com/example/academicpulse/view/pages/publications/one-publication.kt
+++ app/src/main/java/com/example/academicpulse/view/pages/publications/one-publication.kt
@@ -4,19 +4,16 @@ import android.content.Intent
 import android.net.Uri
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -37,30 +34,41 @@ import com.example.academicpulse.view.components.basic.Text
 import com.example.academicpulse.view.components.global.Header
 import com.example.academicpulse.view_model.Store
 import com.example.academicpulse.model.Publication
-import com.example.academicpulse.theme.bottomBarHeight
 import com.example.academicpulse.theme.descriptionTextSize
 import com.example.academicpulse.view.components.basic.Title
 import com.example.academicpulse.view.components.global.Line
 import com.example.academicpulse.utils.context
 import com.example.academicpulse.view.components.basic.Button
 import com.example.academicpulse.view.components.global.AuthorsRow
+import com.example.academicpulse.view.components.global.NotFoundPage
 import com.example.academicpulse.view.components.global.PublicationSettings
 
 @Composable
 fun OnePublicationPage() {
-	val (loading, setLoading) = useState { true }
+	val (loading, setLoading) = useState { false }
+	val (publicationNotFound, setPublicationNotFound) = useState { false }
 	val (publication, setPublication) = useState<Publication?> { null }
 
-	LaunchedEffect(Unit) {
+	fun fetchSelectedPublication() {
+		setLoading(true)
 		Store.publications.fetchSelected(
-			onError = { setLoading(false) },
+			onError = {
+				setLoading(false)
+				setPublicationNotFound(true)
+			},
 			onSuccess = {
 				setPublication(it)
+				setPublicationNotFound(false)
 				setLoading(false)
 			},
 		)
 	}
+	// lunch onMounted
+	LaunchedEffect(Unit) {
+		fetchSelectedPublication()
+	}
 
+	// preventing the user from leaving when it's loading
 	val (back, setBack) = useState { {} }
 	LaunchedEffect(loading) {
 		if (loading) setBack {}
@@ -88,72 +96,82 @@ fun OnePublicationPage() {
 		})
 		Spacer(Modifier.height(14.dp))
 
-		if (loading) Spinner()
-		else if (publication != null) {
-			Spacer(Modifier.height(14.dp))
-			Box(
-				modifier = Modifier
-					.wrapContentSize()
-					.clip(RoundedCornerShape(6.dp))
-					.background(MaterialTheme.colorScheme.primary)
-					.padding(horizontal = 30.dp, vertical = 8.dp), contentAlignment = Alignment.Center
-			) {
-				Text(text = publication.type, color = MaterialTheme.colorScheme.background)
-			}
-			Spacer(Modifier.height(15.dp))
-			H2(text = publication.title)
-
-			// Publication's Date
-			Spacer(Modifier.height(10.dp))
-			Text(text = Publication.formatDate(publication.date))
-
-			if (publication.hasFile) {
-				// Publication's File
-				publication.fetchFile()
-				if (!publication.fileAvailability) {
-					Row {
-						Spacer(Modifier.weight(1f))
-						Spinner(inline = true)
-						Spacer(Modifier.width(4.dp))
-						Text(text = R.string.file_is_loading)
-						Spacer(Modifier.weight(1f))
-					}
-				} else if (publication.file != null) {
-					Spacer(Modifier.height(10.dp))
-					Button(
-						text = R.string.view_file,
-						icon = R.drawable.icon_open_link,
-						modifier = Modifier.wrapContentWidth()
-					) { Router.navigate("publications/pdf-viewer") }
-				}
-			}
-
-			// Publication's DOI
-			if (publication.doi.isNotBlank()) {
-				Spacer(Modifier.height(15.dp))
-				Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
-					Title(text = "DOI")
-					Text(text = publication.doi, size = descriptionTextSize, underlined = true) {
-						val url = "https://www.doi.org/${publication.doi}"
-						val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-						context.startActivity(intent)
-					}
-				}
-			}
-			Spacer(Modifier.height(10.dp))
-
-			// Publication's Authors
-			AuthorsRow(authors = publication.authors, showProfileOnClick = true)
-			Spacer(Modifier.height(15.dp))
-			Line(height = 2.dp)
-			Spacer(Modifier.height(15.dp))
-
-			// Publication's Abstract
-			H2(text = "Abstract")
-			Spacer(Modifier.height(10.dp))
-			Text(text = publication.abstract, size = descriptionTextSize)
-			Spacer(Modifier.height(25.dp))
+		if (loading) {
+			Spinner()
+			return
 		}
+
+		if (publicationNotFound || publication == null) {
+			NotFoundPage(R.string.publication_not_found) {
+				fetchSelectedPublication()
+			}
+			return
+		}
+
+		Spacer(Modifier.height(14.dp))
+		Box(
+			modifier = Modifier
+				.wrapContentSize()
+				.clip(RoundedCornerShape(6.dp))
+				.background(MaterialTheme.colorScheme.primary)
+				.padding(horizontal = 30.dp, vertical = 8.dp),
+			contentAlignment = Alignment.Center
+		) {
+			Text(text = publication.type, color = MaterialTheme.colorScheme.background)
+		}
+		Spacer(Modifier.height(15.dp))
+		H2(text = publication.title)
+
+		// Publication's Date
+		Spacer(Modifier.height(10.dp))
+		Text(text = Publication.formatDate(publication.date))
+
+		if (publication.hasFile) {
+			// Publication's File
+			publication.fetchFile()
+			if (!publication.fileAvailability) {
+				Row {
+					Spacer(Modifier.weight(1f))
+					Spinner(inline = true)
+					Spacer(Modifier.width(4.dp))
+					Text(text = R.string.file_is_loading)
+					Spacer(Modifier.weight(1f))
+				}
+			} else if (publication.file != null) {
+				Spacer(Modifier.height(10.dp))
+				Button(
+					text = R.string.view_file,
+					icon = R.drawable.icon_open_link,
+					modifier = Modifier.wrapContentWidth()
+				) { Router.navigate("publications/pdf-viewer") }
+			}
+		}
+
+		// Publication's DOI
+		if (publication.doi.isNotBlank()) {
+			Spacer(Modifier.height(15.dp))
+			Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+				Title(text = "DOI")
+				Text(text = publication.doi, size = descriptionTextSize, underlined = true) {
+					val url = "https://www.doi.org/${publication.doi}"
+					val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+					context.startActivity(intent)
+				}
+			}
+		}
+		Spacer(Modifier.height(10.dp))
+
+		// Publication's Authors
+		AuthorsRow(authors = publication.authors, showProfileOnClick = true)
+		Spacer(Modifier.height(15.dp))
+		Line(height = 2.dp)
+		Spacer(Modifier.height(15.dp))
+
+		// Publication's Abstract
+		H2(text = "Abstract")
+		Spacer(Modifier.height(10.dp))
+		Text(text = publication.abstract, size = descriptionTextSize)
+		Spacer(Modifier.height(25.dp))
 	}
 
 	BackHandler(onBack = back)
